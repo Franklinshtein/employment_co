fda5a4fad23bc8555c30ec68a037a291d6da57cb
Franklinshtein <98875248+Franklinshtein@users.noreply.github.com>
2024-11-29 20:42:37 +0100

    Page_beta

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const burger = document.getElementById("burger");
-    const menu = document.getElementById("menu");
-
-    // Toggle the menu visibility on burger button click
-    burger.addEventListener("click", () => {
-        menu.classList.toggle("show");  // Toggling the class 'show' to display the menu
-    });
+document.querySelector('.burger').addEventListener('click', function(){
+    this.classList.toggle('active');
+    document.querySelector('.header-nav').classList.toggle('open');
 });
